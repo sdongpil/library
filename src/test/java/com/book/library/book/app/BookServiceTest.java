461fa85457754e7e5a0cc6ed68967f773cf50c94
sdongpil <pildong94@naver.com>
2023-12-11 01:28:06 +0900

    test: BookService - update 테스트 작성

--- src/test/java/com/book/library/book/app/BookServiceTest.java
+++ src/test/java/com/book/library/book/app/BookServiceTest.java
@@ -1,11 +1,15 @@
 package com.book.library.book.app;
 
+import com.book.library.book.domain.Book;
+import com.book.library.book.repository.BookRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -14,9 +18,13 @@ class BookServiceTest {
     @Autowired
     private BookService bookService;
 
-    @Test
-    void t1() {
+    @Autowired
+    private BookRepository bookRepository;
 
+    @Test
+    @Transactional
+    @DisplayName("BookRequest값 유효하면 도서 저장 성공")
+    void t1() {
         BookRequest bookRequest = BookRequest.builder()
                 .name("객체지향의 사실과 오해")
                 .description("")
@@ -26,5 +34,33 @@ class BookServiceTest {
         BookResponse bookResponse = bookService.save(bookRequest);
 
         assertThat(bookResponse.author()).isEqualTo("조영호");
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("도서 id와 BookRequest값 유효하면 도서 수정 성공")
+    void t2() {
+        saveBook();
+
+        BookRequest bookRequest = BookRequest.builder()
+                .name("자바의 신2")
+                .author("이상민2")
+                .description("자바 책2")
+                .build();
+
+        BookResponse bookResponse = bookService.update(1L, bookRequest);
+
+        assertThat(bookResponse.author()).isEqualTo("이상민2");
+    }
+
+
+    private void saveBook() {
+        Book book = Book.builder()
+                .name("자바의 신")
+                .description("자바 책")
+                .author("")
+                .build();
+
+        bookRepository.save(book);
     }
 }
