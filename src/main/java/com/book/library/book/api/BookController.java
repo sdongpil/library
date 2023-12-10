ee713b25b4a8fc85b8ba2926adc42d59e9c126d2
sdongpil <pildong94@naver.com>
2023-12-10 23:22:38 +0900

    feat: BookController - 도서 수정 로직 작성

--- src/main/java/com/book/library/book/api/BookController.java
+++ src/main/java/com/book/library/book/api/BookController.java
@@ -6,10 +6,7 @@ import com.book.library.book.app.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -32,5 +29,35 @@ public class BookController {
                 .build();
 
         return ResponseEntity.status(HttpStatus.CREATED).body(bookResponseDto);
+    }
+
+//    @PutMapping("/books/{bookId}")
+//    public ResponseEntity<BookResponseDto> update(@PathVariable Long bookId, @RequestParam Long memberId) {
+//
+//        BookResponse bookResponse = bookService.update(bookId);
+//
+//        BookResponseDto bookResponseDto = BookResponseDto.builder()
+//                .name(bookResponse.name())
+//                .author(bookResponse.author())
+//                .description(bookResponse.description())
+//                .build();
+//
+//        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(bookResponseDto);
+//    }
+
+    @PutMapping("/books/{id}")
+    public ResponseEntity<BookResponseDto> update(@PathVariable Long id, @RequestBody BookRequestDto bookRequestDto) {
+
+        BookRequest request = BookRequest.toRequest(bookRequestDto);
+
+        BookResponse bookResponse = bookService.update(id, request);
+
+        BookResponseDto bookResponseDto = BookResponseDto.builder()
+                .name(bookResponse.name())
+                .author(bookResponse.author())
+                .description(bookResponse.description())
+                .build();
+
+        return ResponseEntity.ok(bookResponseDto);
     }
 }
