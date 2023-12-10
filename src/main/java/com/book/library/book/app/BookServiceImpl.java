151f425ddee8cd1978a9cfde806339f1aad0c949
sdongpil <pildong94@naver.com>
2023-12-11 01:27:34 +0900

    feat: BookService - update 로직 추가

--- src/main/java/com/book/library/book/app/BookServiceImpl.java
+++ src/main/java/com/book/library/book/app/BookServiceImpl.java
@@ -2,6 +2,7 @@ package com.book.library.book.app;
 
 import com.book.library.book.domain.Book;
 import com.book.library.book.repository.BookRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +13,28 @@ public class BookServiceImpl implements BookService{
     private final BookRepository bookRepository;
 
     @Override
+    @Transactional
     public BookResponse save(BookRequest request) {
 
         Book book = Book.toDomain(request);
+
+        bookRepository.save(book);
+
+        return BookResponse.builder()
+                .name(book.getName())
+                .author(book.getAuthor())
+                .description(book.getDescription())
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public BookResponse update(Long id, BookRequest bookRequest) {
+        Book book = bookRepository.findById(id).orElseThrow(IllegalStateException::new);
+
+        book.setName(bookRequest.name());
+        book.setAuthor(bookRequest.author());
+        book.setDescription(bookRequest.description());
 
         bookRepository.save(book);
 
