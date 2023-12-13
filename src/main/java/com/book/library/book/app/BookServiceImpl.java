4aba31137878789e110281d1368930e09efa4639
sdongpil <pildong94@naver.com>
2023-12-13 21:48:45 +0900

    refactor: 트랜잭션 추가

--- src/main/java/com/book/library/book/app/BookServiceImpl.java
+++ src/main/java/com/book/library/book/app/BookServiceImpl.java
@@ -88,6 +88,7 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    @Transactional
     public List<BookRentResponse> getBookRentHistory(String memberId) {
         List<BookRent> bookRents = bookRentRepository.findAllByMemberId(memberId);
 
